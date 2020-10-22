1ca8aebd83304734205d0ff53aacf416de81d4ad
Jean Mendonça <pierrybm@gmail.com>
2020-10-22 16:19:05 +0000

    including the map on the screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Fragment } from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
 const App = () => {
+  const { REACT_APP_GOOGLE_API_KEY } = process.env
   return (
     <Fragment>
-      ...
+      <LoadScript googleMapsApiKey={ REACT_APP_GOOGLE_API_KEY }>
+        <GoogleMap
+          mapContainerStyle={{ height: '100vh', width: '100%' }}
+          zoom={ 15 }
+          center={{ lat: -21.7768606, lng: -41.3109657 }}
+        >
+        
+        </GoogleMap>
+      </LoadScript>
     </Fragment>
   )
 }
