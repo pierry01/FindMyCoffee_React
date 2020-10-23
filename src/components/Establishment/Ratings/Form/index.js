af64a069c960aa84927a7aeb3223212f0b75d7f4
Jean Mendonça <pierrybm@gmail.com>
2020-10-23 21:14:49 +0000

    including the review list

--- src/components/Establishment/Ratings/Form/index.js
+++ src/components/Establishment/Ratings/Form/index.js
@@ -1,17 +1,43 @@
 import React, { useState } from 'react'
 import { Container, Input, TextArea, Button } from './styles'
 import ReactStars from 'react-rating-stars-component'
+import RatingService from '../../../../services/rating'
 
-const Form = () => {
+const Form = ({ place, loadStore }) => {
   const [name, setName] = useState('')
   const [message, setMessage] = useState('')
   const [value, setValue] = useState(1)
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const store_params = {
+      latitude: place.geometry.location.lat,
+      longitude: place.geometry.location.lng,
+      name: place.name,
+      address: place.formatted_address,
+      google_place_id: place.place_id
+    }
+
+    const rating_params = {
+      value: (value == null) ? 1 : value,
+      opinion: message,
+      user_name: name
+    }
+
+    await RatingService.create(store_params, rating_params)
+
+    loadStore()
+
+    setName('')
+    setMessage('')
+  }
 
   return (
     <Container>
       <h4>Deixe sua Opinião</h4>
       
-      <form autoComplete='off'>
+      <form autoComplete='off' onSubmit={handleSubmit}>
         <Input
           type='text'
           name='name'
--- src/components/Establishment/Ratings/index.js
+++ src/components/Establishment/Ratings/index.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container } from './styles'
 import Form from './Form'
+import StoreService from '../../../services/store'
+import ReactStars from 'react-rating-stars-component'
 
 const Ratings = ({ place }) => {
+  const [store, setStore] = useState([])
+
+  // eslint-disable-next-line
+  useEffect(() => { loadStore() }, [place])
+
+  const loadStore = async () => {
+    setStore([])
+
+    try {
+      const response = await StoreService.show(place.place_id)
+      setStore(response.data)
+    } catch (e) { 
+      setStore([]) 
+      console.log(e)
+    }
+  }
+
   return (
     <Container>
-      <Form place={ place } />
+      <h4>
+        { store.ratings_count || 0 } Opiniões
+
+        { 
+          store.ratings_average && 
+            <ReactStars edit={ false } value={ store.ratings_average || 0 } /> 
+        }
+      </h4>
+      
+      <hr />
+      
+      {
+        store.ratings &&
+          <div>
+            {
+              store.ratings.map((rating, key) => {
+                return(
+                  <div key={ key }>
+                    <strong>{ rating.user_name }</strong>
+                    <ReactStars edit={ false } value={ rating.value } />
+                    <p>{ rating.opinion }</p>
+                    <p>{ rating.date }</p>
+                    <hr />
+                  </div>
+                )
+              })
+            }
+          </div>
+      }
+
+      <Form place={ place } loadStore={ loadStore } />
     </Container>
   )
 }
